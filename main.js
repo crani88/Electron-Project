0798afa66f2d49aeaa6659c6eedb2d3ac3c914f5
Andreas Hedbäck <andhe808@student.liu.se>
2018-07-13 22:37:47 +0200

    Implemented support for sqlite3. Synced Node.js version with electron, added rebuild script to electron app.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile(__dirname + '/app/index.html')
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
