0db5900199f0b37017abb4b8a7ea126207152196
Andreas Hedbäck <andhe808@student.liu.se>
2018-07-14 02:05:53 +0200

    Fixed the content swapping by menu items. Introduced a dev_notes .txt file.

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,12 +1,16 @@
+const fs = require("fs");
+const mainWindow = document.getElementById("MainWindow");
+
 const MAIN_MENU = {
-    "home" : "<a href='#home'>Home</a>",
-    "profile" : "<a href='#profile'>Profile</a>",
-    "friends" : "<li><a href='#friends'>Friends</a>",
-    "logout" : "<a href='#logout'>Logout</a>"
+    "home" : '<a onclick=changeContent("home") href="javascript:void(0);">Home</a>',
+    "profile" : '<a onclick=changeContent("profile") href="javascript:void(0);">Profile</a>',
+    "friends" : '<li><a onclick=changeContent("friends") href="javascript:void(0);">Friends</a>',
+    "logout" : '<a onclick=changeContent("logout") href="javascript:void(0);">Logout</a>'
 };
 
 create_main_menu = function() {
     let user_image = document.createElement("img");
+    user_image.src = './img/ranks/Gold/rank018.png';
     let link = document.getElementById("nav_menu");
     link.appendChild(user_image);
 
@@ -17,7 +21,33 @@ create_main_menu = function() {
         child.id = key;
         link.appendChild(child);
     });
+
+    let myNotification = new Notification('Superspelet', {
+        body: 'Started new session'
+      })
 }
+
+changeContent = function(view) {
+    console.log(view);
+    switch(view) {
+        case "home":
+            mainWindow.innerHTML = fs.readFileSync('./app/html/welcome.html');
+            break;
+        case "profile":
+            mainWindow.innerHTML = fs.readFileSync('./app/html/profile.html');
+            break;
+        case "friends":
+            mainWindow.innerHTML = fs.readFileSync('./app/html/friends.html');
+            break;
+        case "logout":
+            mainWindow.innerHTML = fs.readFileSync('./app/html/logout.html');
+            break;
+        default:
+            mainWindow.innerHTML = fs.readFileSync('./app/html/welcome.html');
+            break;
+    }
+}
+
 create_main_menu();
 
 /*
